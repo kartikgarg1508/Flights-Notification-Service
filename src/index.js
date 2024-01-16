856ed49309a60aae8276eb3da0298a2f0a2b6275
kartikgarg1508 <kartikgarg394@gmail.com>
2024-01-17 02:12:09 +0530

    fix: request body undefined

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ const app = express();
 const { ServerConfig, LoggerConfig } = require("./config");
 const apiRoutes = require("./routes");
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 app.use("/api", apiRoutes);
 
 app.listen(ServerConfig.PORT, () => {
