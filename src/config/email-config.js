339e1f6a8453794c845429e2fd2e4581cd758698
kartikgarg1508 <kartikgarg394@gmail.com>
2024-01-29 00:52:44 +0530

    feat: configured nodemailer

---
+++ src/config/email-config.js
@@ -0,0 +1,12 @@
+const nodemailer = require("nodemailer");
+const { GMAIL_APP_PASSWORD, GMAIL_EMAIL } = require("./server-config");
+
+const mailsender = nodemailer.createTransport({
+  service: "Gmail",
+  auth: {
+    user: GMAIL_EMAIL,
+    pass: GMAIL_APP_PASSWORD,
+  },
+});
+
+module.exports = mailsender;
--- src/config/server-config.js
+++ src/config/server-config.js
@@ -4,4 +4,6 @@ dotenv.config();
 
 module.exports = {
   PORT: process.env.PORT,
+  GMAIL_APP_PASSWORD: process.env.GMAIL_APP_PASSWORD,
+  GMAIL_EMAIL: process.env.GMAIL_EMAIL,
 };
